4d525f02347bf50d3c02d29e57b168ae9ecce622
Max <maxandrewwhittle@gmail.com>
2021-01-16 14:43:39 +0000

    Plural Commit

--- words.py
+++ words.py
@@ -1,6 +1,12 @@
-from os import rename
+from urllib.request import urlopen
 
-import requests
+story = urlopen("http://sixty-north.com/c/t.txt")
+story_words = []
+for line in story:
+    line_words = line.decode("utf-8").split()
+    for word in line_words:
+        story_words.append(word)
+story.close()
 
-r = requests.get("https://coreyms.com")
-print(r.status_code)
+for word in story_words:
+    print(word)
